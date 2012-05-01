38f882a449275d05f4115f0718013be7dfadabcc
Max Korenkov <mkorenkov@gmail.com>
2012-05-01 22:30:37 +0300

    worker stub

---
+++ worker/lib/worker.rb
@@ -0,0 +1,10 @@
+# vim: filetype=ruby
+# vim: tabstop=2 expandtab
+# encoding: utf-8
+#
+# tow_server.rb
+#
+# Generic worker
+
+class Worker
+end
---
+++ worker/lib/worker/receive_label_list.rb
@@ -0,0 +1,10 @@
+# vim: filetype=ruby
+# vim: tabstop=2 expandtab
+# encoding: utf-8
+#
+# tow_server.rb
+#
+# Retrieves Label List
+
+class ReceiveLabelList
+end
